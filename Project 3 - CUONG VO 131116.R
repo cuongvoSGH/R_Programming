7c147864049107c8fc3e569b34f4d6eca2c35554
cuongvoSGH <cuongvo.polnd@gmail.com>
2023-11-06 10:19:01 +0100

    Integrated 3D

--- Project 3 - CUONG VO 131116.R
+++ Project 3 - CUONG VO 131116.R
@@ -1,3 +1,32 @@
 # Cuong Vo - 131116
 
 # Algorithms Documentation
+
+integrate3d <- function(f, over, n){
+  x <- seq(from = over$x[[1]], to = over$x[[2]], length.out = n) 
+  y <- seq(from = over$y[[1]], to = over$y[[2]], length.out = n)
+  z <- seq(from = 0, to = pi, length.out = n)
+  result = 0
+  
+  for (i in (1:n)){
+    x1 <- sample(x, size = 1)
+    y1 <- sample(y, size = 1)
+    z1 <- sample(z, size = 1)
+    
+    z_actual <- f(x1,y1)
+    
+    if (z1 <= z_actual) result = result + 1
+  }
+  return(result)
+}
+
+debug(fun = integrate3d)
+integrate3d(f = function(x, y) { (cos(x) + 2) * (sin(y) + 1)},
+            over = list(x = c(0, pi), y = c(0, pi)),
+            n = 10^5
+            )
+
+
+
+
+
