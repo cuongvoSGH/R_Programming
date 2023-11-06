34b3b3e8a25e7fd57db0061097378d5a3fcbeeae
cuongvoSGH <cuongvo.polnd@gmail.com>
2023-11-06 13:38:22 +0100

    Pro 3 completed

--- Project 3 - CUONG VO 131116.R
+++ Project 3 - CUONG VO 131116.R
@@ -3,26 +3,26 @@
 # Algorithms Documentation
 
 integrate3d <- function(f, over, n){
-  x <- seq(from = over$x[[1]], to = over$x[[2]], length.out = n) 
-  y <- seq(from = over$y[[1]], to = over$y[[2]], length.out = n)
-  z <- seq(from = 0, to = pi, length.out = n)
-  result = 0
+  x <- seq(from = over$x[[1]], to = over$x[[2]], length.out = if (n > 10^3) {10^3} else {n}) 
+  y <- seq(from = over$y[[1]], to = over$y[[2]], length.out = if (n > 10^3) {10^3} else {n})
+  x1 <- sample(x, size = n, replace = TRUE)
+  y1 <- sample(y, size = n, replace = TRUE)
+  z_actual <- f(x1,y1)
+  z_min <- min(z_actual)
+  z_min <- min(z_min, over$x[[1]], over$y[[1]])
+  z_max <- max(z_actual)
+  z_max <- max(z_max, over$x[[2]], over$y[[2]])
+  z <- seq(from = z_min, to = z_max, length.out = if (n > 10^3) {10^3} else {n}) 
+  z1 <- sample(z, size = n, replace = TRUE)
   
-  for (i in (1:n)){
-    x1 <- sample(x, size = 1)
-    y1 <- sample(y, size = 1)
-    z1 <- sample(z, size = 1)
-    
-    z_actual <- f(x1,y1)
-    
-    if (z1 <= z_actual) result = result + 1
-  }
-  return(result)
+  
+  z_actual <- z_actual - z1
+  result <- length(z_actual[z_actual >= 0])
+  return(result / n * (over$x[[2]] - over$x[[1]]) * (over$y[[2]] - over$y[[1]]) * (z_max - z_min))
 }
 
-debug(fun = integrate3d)
-integrate3d(f = function(x, y) { (cos(x) + 2) * (sin(y) + 1)},
-            over = list(x = c(0, pi), y = c(0, pi)),
+integrate3d(f = function(x, y) {cos(x) * y},
+            over = list(x = c(0, pi / 2), y = c(0, 1)),
             n = 10^5
             )
 
